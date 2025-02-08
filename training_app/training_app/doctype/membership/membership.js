c177f7369b3fc7887e9f15d2ba9a69808b81d6e9
piyushshindegit <153705609+piyushshindegit@users.noreply.github.com>
2025-02-08 21:32:50 +0530

    just for demo purpose

---
+++ training_app/training_app/doctype/membership/membership.js
@@ -0,0 +1,12 @@
+
+
+
+
+// Copyright (c) 2025, piyush and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("membership", {
+// 	refresh(frm) {
+
+// 	},
+// });
